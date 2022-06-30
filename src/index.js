e41ad0e9d75df0f372f16c5419d2ca0d7a775960
cvzi <cuzi@openmail.cc>
2022-06-30 12:04:36 +0200

    Add geoJSON support Add Points of interest Respect language settings Option to clear cache

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-/* globals GM, GM_setClipboard, sessionStorage, Blob */
+/* globals GM, sessionStorage, Blob */
 
 import Picker from 'vanilla-picker'
+
+const PROGRESS_MAX = 14
+const HL = getSiteLanguage()
 
 document.head.appendChild(document.createElement('style')).innerHTML = `
 .btn-download {
@@ -48,14 +51,18 @@ document.head.appendChild(document.createElement('style')).innerHTML = `
   display:inline;
   width:50px;
 }
-
+.main-popup-window select {
+  display:inline;
+  width:auto;
+  -webkit-appearance: auto;
+  appearance: auto;
+}
 .main-popup-window fieldset {
   border: 1px #a204fb solid;
   margin: 3px;
   padding: 2px;
   background: #f4ebff;
 }
-
 
 `
 
@@ -65,6 +72,7 @@ if (document.location.href.match(/\/r\/(\d+)/)) {
 
 function addDownloadButtons () {
   GM.registerMenuCommand('Download route file', () => downloadRoute())
+  GM.registerMenuCommand('Clear cache', () => clearCache())
 
   const div = document.querySelector('.btn-group .actions').appendChild(document.createElement('div'))
 
@@ -90,7 +98,7 @@ function downloadRoute (format) {
   div.classList.add('main-popup-window')
   const progress = div.appendChild(document.createElement('progress'))
   progress.value = 0
-  progress.max = 12
+  progress.max = PROGRESS_MAX
   progress.style.visibility = 'hidden'
 
   div.appendChild(document.createElement('br'))
@@ -98,10 +106,17 @@ function downloadRoute (format) {
   const label0 = div.appendChild(document.createElement('label'))
   label0.setAttribute('for', 'export_format')
   label0.appendChild(document.createTextNode('Export format:'))
-  const input = div.appendChild(document.createElement('input'))
-  input.setAttribute('id', 'export_format')
-  input.value = format || 'gpx'
-  div.appendChild(document.createTextNode(' (kml or gpx or tcx)'))
+  const dropDown = div.appendChild(document.createElement('select'))
+  dropDown.setAttribute('id', 'export_format')
+  const formats = ['gpx', 'kml', 'tcx', 'geojson']
+  const presetFormat = format || 'gpx'
+  formats.forEach(function (ext) {
+    const option = dropDown.appendChild(document.createElement('option'))
+    option.appendChild(document.createTextNode(ext))
+    if (presetFormat === ext) {
+      option.selected = true
+    }
+  })
 
   div.appendChild(document.createElement('br'))
 
@@ -123,7 +138,7 @@ function downloadRoute (format) {
 
   const fieldset = div.appendChild(document.createElement('fieldset'))
   const legend = fieldset.appendChild(document.createElement('legend'))
-  legend.appendChild(document.createTextNode('KML options'))
+  legend.appendChild(document.createTextNode('KML/geoJSON options'))
 
   const labelColor = fieldset.appendChild(document.createElement('label'))
   labelColor.setAttribute('for', 'kml_line_color')
@@ -188,7 +203,7 @@ function startDownload (progress) {
   const routeDescriptionDiv = document.querySelector('.route-description')
   const desc = routeDescriptionDiv ? routeDescriptionDiv.textContent.trim() : ''
 
-  const format = document.getElementById('export_format').value.trim().toLowerCase()
+  const format = document.getElementById('export_format').selectedOptions[0].value.trim().toLowerCase()
   const addElevation = document.getElementById('export_add_elevation').checked
   const lineColor = document.getElementById('kml_line_color').value
   const lineWidth = document.getElementById('kml_line_width').value
@@ -203,6 +218,9 @@ function downloadVertices (routeId, format, addElevation, lineColor, lineWidth, 
   cachedRequest({
     method: 'GET',
     url: `https://maptoolkit.net/export/outdoorish_bikemap_routes/${routeId}.profile?api_key=outdoorish`,
+    headers: {
+      'x-requested-with': 'XMLHttpRequest'
+    },
     onload: function (resp) {
       progress.value = 2
       const routeData = JSON.parse(resp.responseText)
@@ -213,16 +231,54 @@ function downloadVertices (routeId, format, addElevation, lineColor, lineWidth, 
       routeData.addElevation = !!addElevation
       routeData.lineColor = lineColor
       routeData.lineWidth = lineWidth
-      if (routeData.addElevation) {
-        downloadElevation(routeData, progress, 0)
-      } else {
-        exportRoute(routeData, progress)
-      }
+      routeData.pois = []
+      downloadPOIs(routeData, progress)
     },
     onerror: function (response) {
       window.alert('Error:' + response.status)
     }
   })
+}
+
+function downloadPOIs (routeData, progress) {
+  console.log('downloadPOIs()')
+  progress.value = 3
+  cachedRequest({
+    method: 'GET',
+    url: `https://www.bikemap.net/${HL}/r/${routeData.routeId}/data/`,
+    headers: {
+      'x-requested-with': 'XMLHttpRequest'
+    },
+    referrer: `https://www.bikemap.net/${HL}/r/11276904/`,
+    onload: function (resp) {
+      progress.value = 4
+      try {
+        const data = JSON.parse(resp.responseText)
+        if ('pois' in data) {
+          const pois = JSON.parse(data.pois)
+          if (pois) {
+            routeData.pois = pois
+          }
+        }
+      } catch (e) {
+        window.alert('Could not download "Point of Interests". Continuing without them\n\nError:\n' + e)
+        console.error(e)
+      }
+      decideIfElevation(routeData, progress)
+    },
+    onerror: function (response) {
+      decideIfElevation(routeData, progress)
+      window.alert('Error:' + response.status)
+    }
+  })
+}
+
+function decideIfElevation (routeData, progress) {
+  if (routeData.addElevation) {
+    downloadElevation(routeData, progress, 0)
+  } else {
+    exportRoute(routeData, progress)
+  }
 }
 
 function downloadElevation (routeData, progress, index) {
@@ -232,11 +288,14 @@ function downloadElevation (routeData, progress, index) {
   const start = index || 0
   const end = start + n < routeData.vertices.length ? start + n : routeData.vertices.length
 
-  progress.value = 2 + 10 * (start / routeData.vertices.length)
+  progress.value = 4 + 10 * (start / routeData.vertices.length)
 
   cachedRequest({
     method: 'GET',
     url: `https://maptoolkit.net/elevationprofile?points=${JSON.stringify(routeData.vertices.slice(start, end))}&tolerance=10&api_key=outdoorish`,
+    headers: {
+      'x-requested-with': 'XMLHttpRequest'
+    },
     onload: function (resp) {
       const data = JSON.parse(resp.responseText)
       // Add elevation data to points if available
@@ -285,22 +344,46 @@ function exportRoute (routeData, progress) {
     toGPX(routeData, progress)
   } else if (routeData.format === 'tcx') {
     toTCX(routeData, progress)
+  } else if (routeData.format === 'geojson') {
+    toGeoJSON(routeData, progress)
   } else {
     toKML(routeData, progress)
   }
 }
 
 function toKML (routeData, progress) {
-  progress.value = 12
-  const coordinates = routeData.vertices.map(p => p.reverse().join(',')).join('\n')
-  // TODO set color
+  progress.value = PROGRESS_MAX
 
+  const poiXML = routeData.pois.map(poi => {
+    const name = poi.text || (HL === 'en' ? 'Point of Interest' : 'Interessanter Ort')
+    let icon = ''
+    if ('icon' in poi && poi.icon && 'iconUrl' in poi.icon) {
+      icon = `<Icon>
+        <href>https://static.bikemap.net/${escapeXML(poi.icon.iconUrl)}</href>
+      </Icon>`
+    }
+    let description = poiType(poi.poi_class, HL)
+    if ('image' in poi && poi.image) {
+      description += '\n\n' + poi.image
+    }
+
+    return `<Placemark>
+      <name>${escapeXML(name)}</name>
+      <description>${escapeXML(description)}</description>
+      ${icon}
+      <Point>
+         <coordinates>${poi.lng},${poi.lat}</coordinates>
+       </Point>
+    </Placemark>`
+  }).join('\n    ')
+
+  const coordinates = routeData.vertices.map(p => p.reverse().join(',')).join('\n')
   const temp = `<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
   <Document>
     <name>${escapeXML(routeData.routeName)}</name>
     <description>${escapeXML(routeData.routeDesc)}
-    https://www.bikemap.net/en/r/${escapeXML(routeData.routeId)}/
+    https://www.bikemap.net/${HL}/r/${escapeXML(routeData.routeId)}/
 ${escapeXML(metablock(routeData))}
     </description>
     <Style id="myStyle">
@@ -324,15 +407,35 @@ ${escapeXML(metablock(routeData))}
         </coordinates>
       </LineString>
     </Placemark>
+    ${poiXML}
   </Document>
 </kml>
 `
-  // GM_setClipboard(temp)
+
   downloadFile(routeData, fileName(routeData.routeName + '_' + routeData.routeId, 'kml'), temp, 'application/vnd.google-earth.kml+xml', progress)
 }
 
 function toGPX (routeData, progress) {
-  progress.value = 12
+  progress.value = PROGRESS_MAX
+
+  const wtes = routeData.pois.map(poi => {
+    const name = poi.text || (HL === 'en' ? 'Point of Interest' : 'Interessanter Ort')
+    const type = poiType(poi.poi_class, HL)
+    let description = type
+    let link = ''
+    if ('image' in poi && poi.image) {
+      description += '\n\n' + poi.image
+      link = `<link href="${escapeXML(poi.image)}"><text></text></link>`
+    }
+    return `
+    <wpt lat="${poi.lat}" lon="${poi.lng}">
+      <name>${escapeXML(name)}</name>
+      <desc>${escapeXML(description)}</desc>
+      ${link}
+      <sym>${escapeXML(poiType(poi.poi_class, 'gpx'))}</sym>
+      <type>${type}</type>
+    </wpt>`
+  }).join('\n')
 
   const rtepts = routeData.vertices.map(p => {
     if (p.length === 3) {
@@ -351,23 +454,42 @@ function toGPX (routeData, progress) {
       <desc>
       ${escapeXML(metablock(routeData))}
       </desc>
-      <link href="https://www.bikemap.net/en/r/${escapeXML(routeData.routeId)}/">
+      <link href="https://www.bikemap.net/${HL}/r/${escapeXML(routeData.routeId)}/">
         <text>${escapeXML(routeData.routeDesc)}</text>
       </link>
       <time>${escapeXML(new Date().toISOString())}</time>
     </metadata>
+${wtes}
     <rte>
       <name>${escapeXML(routeData.routeName)}</name>
 ${rtepts}
     </rte>
   </gpx>
 `
-  GM_setClipboard(temp)
+
   downloadFile(routeData, fileName(routeData.routeName + '_' + routeData.routeId, 'gpx'), temp, 'application/gpx+xml', progress)
 }
 
 function toTCX (routeData, progress) {
-  progress.value = 12
+  progress.value = PROGRESS_MAX
+
+  const coursePoints = routeData.pois.map(poi => {
+    const name = poi.text || (HL === 'en' ? 'Point of Interest' : 'Interessanter Ort')
+    const type = poiType(poi.poi_class, HL)
+    let description = name + '\n' + type
+    if ('image' in poi && poi.image) {
+      description += '\n\n' + poi.image
+    }
+    return `          <CoursePoint>
+            <Name>${escapeXML(name).substring(0, 10)}</Name>
+            <Position>
+              <LatitudeDegrees>${poi.lat}</LatitudeDegrees>
+              <LongitudeDegrees>${poi.lng}</LongitudeDegrees>
+            </Position>
+            <PointType>${escapeXML(poiType(poi.poi_class, 'tcx'))}</PointType>
+            <Notes>${escapeXML(description)}</Notes>
+          </CoursePoint>`
+  }).join('\n')
 
   let beginPosition
   if (routeData.vertices[0].length === 3) {
@@ -417,9 +539,6 @@ ${content}
     <Courses>
       <Course>
         <Name>${escapeXML(routeData.routeName)}</Name>
-        <Notes>
-        ${escapeXML(metablock(routeData))}
-        </Notes>
         <Lap>
           <DistanceMeters>${routeData.distance}</DistanceMeters>
           <BeginPosition>
@@ -434,12 +553,84 @@ ${content}
         <Track>
 ${trackPoints}
         </Track>
+        <Notes>
+        ${escapeXML(metablock(routeData))}
+        </Notes>
+${coursePoints}
       </Course>
     </Courses>
   </TrainingCenterDatabase>
 `
-  GM_setClipboard(temp)
+
   downloadFile(routeData, fileName(routeData.routeName + '_' + routeData.routeId, 'tcx'), temp, 'application/vnd.garmin.tcx+xml', progress)
+}
+
+function toGeoJSON (routeData, progress) {
+  progress.value = PROGRESS_MAX
+
+  let minElevationRepr = '' + elevationFormat(routeData.minElevation)
+  if (routeData.minElevationPoint) {
+    minElevationRepr += ' (at ' + routeData.minElevationPoint.join(', ') + ')'
+  }
+  let maxElevationRepr = '' + elevationFormat(routeData.maxElevation)
+  if (routeData.maxElevationPoint) {
+    maxElevationRepr += ' (at ' + routeData.maxElevationPoint.join(', ') + ')'
+  }
+
+  const geoJSON = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        properties: {
+          name: routeData.routeName,
+          desc: metablock(routeData),
+          stroke: '#' + routeData.lineColor.substring(6, 8) + routeData.lineColor.substring(4, 6) + routeData.lineColor.substring(2, 4) + routeData.lineColor.substring(0, 2),
+          'stroke-width': routeData.lineWidth,
+          url: `https://www.bikemap.net/${HL}/r/${escapeXML(routeData.routeId)}/`,
+          distance: distanceFormat(routeData.distance),
+          distanceMeters: routeData.distance,
+          timeCycling: hoursMinutes(routeData.cycling_time),
+          timeCyclingRace: hoursMinutes(routeData.race_cycling_time),
+          timeCyclingOffroad: hoursMinutes(routeData.offroad_cycling_time),
+          timeWalking: hoursMinutes(routeData.walking_time),
+          ascent: elevationFormat(routeData.ascend),
+          descend: elevationFormat(routeData.descend),
+          elevationMin: minElevationRepr,
+          elevationMax: maxElevationRepr
+        },
+        geometry: {
+          type: 'LineString',
+          coordinates: routeData.vertices.map(p => p.reverse()) // [lon, lat, alt]
+        },
+        title: routeData.routeName,
+        id: routeData.routeId
+      }
+    ]
+  }
+
+  geoJSON.features.push(...routeData.pois.map(function (poi) {
+    const featurePoint = {
+      type: 'Feature',
+      properties: {
+        name: poi.text || (HL === 'en' ? 'Point of Interest' : 'Interessanter Ort'),
+        desc: poiType(poi.poi_class, HL),
+        'marker-symbol': poiType(poi.poi_class, 'geojson')
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [poi.lng, poi.lat]
+      },
+      title: poi.text || (HL === 'en' ? 'Point of Interest' : 'Interessanter Ort')
+    }
+
+    if ('image' in poi && poi.image) {
+      featurePoint.properties.url = poi.image
+    }
+    return featurePoint
+  }))
+
+  downloadFile(routeData, fileName(routeData.routeName + '_' + routeData.routeId, 'geojson'), JSON.stringify(geoJSON, null, 2), 'application/geo+json', progress)
 }
 
 function metablock (routeData) {
@@ -487,10 +678,61 @@ function downloadFile (routeData, name, content, mimeType, progress) {
   addCloseButton(div)
 }
 
+function poiType (poiClass, lang) {
+  const data = {
+    'poi-hotel': {
+      en: 'Accomodation',
+      de: 'Unterkunft',
+      gpx: 'Hotel',
+      tcx: 'Generic',
+      geojson: 'lodging'
+    },
+    'poi-sight': {
+      en: 'Point of Interest',
+      de: 'Interessanter Ort',
+      tcx: 'Generic',
+      geojson: 'marker'
+    },
+    'poi-repair': {
+      en: 'Bike Service',
+      de: 'Fahrradservice',
+      tcx: 'Generic',
+      geojson: 'bicycle'
+    },
+    'poi-photo': {
+      en: 'Photo',
+      de: 'Foto',
+      gpx: 'Scenic Area',
+      tcx: 'Generic',
+      geojson: 'camera'
+    },
+    'poi-water': {
+      en: 'Drinking Water',
+      de: 'Trinkwasser',
+      gpx: 'Drinking Water',
+      tcx: 'Water',
+      geojson: 'water'
+    }
+  }
+  if (poiClass in data && lang in data[poiClass]) {
+    return data[poiClass][lang]
+  }
+  if (poiClass in data && 'en' in data[poiClass]) {
+    return data[poiClass].en
+  }
+  return poiClass.replace('poi-')
+}
+
 function fileName (title, ext) {
   let name = title.replace(/[:*?<>/\\,|\u0000]/g, '') // eslint-disable-line no-control-regex
   name = name.trim().replace(/^\.+/, '').replace(/\.+$/, '').trim()
   return name + '.' + ext
+}
+
+function clearCache () {
+  const n = sessionStorage.length
+  sessionStorage.clear()
+  window.alert('Cache cleared!\n\n' + n + ' item(s) removed.')
 }
 
 function cachedRequest (obj) {
@@ -562,6 +804,15 @@ function getSiteUnitFormat () {
     return 'imperial'
   } else {
     return 'metric'
+  }
+}
+
+function getSiteLanguage () {
+  const input = document.querySelector('#site-settings-column input[name=language]')
+  if (input && input.value === 'de') {
+    return 'de'
+  } else {
+    return 'en'
   }
 }
 
